664095b7c7c79501eab489db6cd26ac85eb04a5b
Lim Guowei <iamapokemonmaster@gmail.com>
2025-05-01 05:40:57 +0800

    Added content to main container to prepare for CSS of navbar and main container

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useAuthContext } from '../context/AuthContext'
+import './MainContainer.css';
 
 export default function MainContainer() {
   const {session} = useAuthContext()
+  const [authLoading, setAuthLoading] = useState(false)
+  
+  console.log("Session in maincontainer:", session)
   return (
-    <div>Welcome, {session?.user?.email}!</div>
+    <div>
+      <div>Welcome, {session?.user?.email}!</div>
+      <div className='task-list'>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+        <div>Task: Do something</div>
+      </div>
+    </div>
   )
 }
