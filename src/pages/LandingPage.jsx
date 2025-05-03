036f05057b3d1efa8dec3335b2aa5f90aa0541a6
Lim Guowei <iamapokemonmaster@gmail.com>
2025-05-03 11:31:42 +0800

    Refactor Landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
-import Navbar from '../components/LandingNavbar'
-import HeroSection from '../components/HeroSection'
+import { Link } from 'react-router-dom'
+import { useAuthContext } from '../context/AuthContext'
+import './LandingPage.css'
+import Logo from '../components/Logo'
 
 export default function LandingPage() {
+  const {session} = useAuthContext()
+
   return (
     <div>
-        <Navbar/>
-        <HeroSection/>
+      <nav className="navbar">
+        <div className="logo-title">
+            <Logo className="logo"/>
+            <p>Prodigy</p>
+        </div>
+        <ul className='center-nav'>
+            <li>Product</li>
+            <li>Order</li>
+            <li>Pricing</li>
+        </ul>
+        <ul className="right-nav">
+            <li>Learn</li>
+            <li>Support</li>
+            { session ? <li><Link to="/account">My Account</Link></li> : null }
+            <li><Link to="/login">Login</Link></li>{/*  TODO: MAKE IT SWITCH BETWEEN SIGN UP AND LOGIN */}
+        </ul>
+      </nav>
+      <div className='hero-container'>
+        <div className='left-section'>
+            <div>Main Marketing Slogan</div>
+            <div>Sub Marketing Slogan</div>
+            <div><Link to="/login">Start for Free</Link></div>
+        </div>
+        <div className='right-section'>
+            <img src="https://img.freepik.com/free-psd/college-template-design_23-2151693602.jpg?ga=GA1.1.1152302962.1743735256&semt=ais_hybrid&w=740"></img>
+        </div>
+      </div>
     </div>
   )
 }
