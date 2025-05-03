203fa61edf6b43120b8a751ec604f0acade1e197
Lim Guowei <iamapokemonmaster@gmail.com>
2025-05-03 11:12:51 +0800

    Added funtionality for clicking on logo to return to home.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-// import {ReactComponent as LogoSVG} from '../assets/Logo.svg'
+import { Link } from 'react-router-dom'
 import LogoSVG from "../assets/Logo.svg"
 import "./Logo.css"
 
 export default function Logo({size=50, text=true}) {
   return (
-    <div>
-        <img 
-            className='logo__image'
-            src={LogoSVG} 
-            alt="Logo"
-            style={{width: `${size}px`, height:`${size}px`}}
-        />
-        {text ? <span className='logo-text'>Prodigy</span> : null}
-    </div>
+    <Link className="logo__link" to="/home">
+      <div>
+          <img 
+              className='logo__image'
+              src={LogoSVG} 
+              alt="Logo"
+              style={{width: `${size}px`, height:`${size}px`}}
+          />
+          {text ? <span className='logo-text'>Prodigy</span> : null}
+      </div>
+    </Link>
   )
 }
